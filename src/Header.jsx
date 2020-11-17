308e2079fe2fc605fdaf19f2b1aee1707636b688
Aravind5Mukundam <mukundamaravind@gmail.com>
2020-11-17 13:30:10 +0530

    Header changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,9 @@
 // import { Button, Switch } from '@material-ui/core';
 import React from 'react';
 import logo from './Images/logo.png'
-import Login from './Login';
-import Signup from './Signup';
-import {Route, Switch} from 'react-router-dom';
+// import Login from './Login';
+// import Signup from './Signup';
+// import {Route, Switch} from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -12,10 +12,10 @@ const Header = () => {
             
             <img  src={logo} alt ="logo" width='70'  height='60'/>
             <h1 className="h1">To Do List</h1>
-            <Switch>
+            {/* <Switch>
               <Route exact path ='/login' component={Login}/>
               <Route exact path ='/signup' component={Signup}/>
-            </Switch>
+            </Switch> */}
                 
 
           </div>
--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import App from './App';
-// import Login from './Login';
+import Login from './Login';
 // import Signup from './Signup';
 import Header from './Header';
 import Footer from './Footer';
+import {Route, Switch} from 'react-router-dom';
 
 
 
 const Todolist = () => {
     return(
         <>  
+            <Switch>
+              <Route exact path ='/login' component={Login}/>
+              {/* ?<Route exact path ='/signup' component={Signup}/> */}
+            </Switch>
             <Header/>
             <App/>
             <Footer/>
